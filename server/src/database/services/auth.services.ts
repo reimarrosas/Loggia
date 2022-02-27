55815696a871fbb698df13a6df73cbd856623ff9
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-27 13:15:32 -0500

    Edited the UserCredentials interface to properly reflect the entity in the db fixed the bug where the db query is not awaited in the findUserByEmail auth service

--- server/src/database/services/auth.services.ts
+++ server/src/database/services/auth.services.ts
@@ -2,17 +2,17 @@ import { HttpInternal } from '../../utils/HttpErrors/HttpInternal';
 import { db } from '../setup';
 
 export interface UserCredentials {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
+  user_id: number;
+  user_name: string;
+  user_email: string;
+  user_password: string;
 }
 
 export const findUserByEmail = async (
   email: string
 ): Promise<UserCredentials | null> => {
   try {
-    return db.oneOrNone(
+    return await db.oneOrNone(
       `SELECT user_id, user_name, user_email, user_password FROM users WHERE user_email = $1`,
       [email]
     );
