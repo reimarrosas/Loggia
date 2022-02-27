414034f3f840a5ad2ec9ee7af63d70f0d85c7a56
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-27 10:41:15 -0500

    Added user_password for the result of querying user by email

--- server/src/database/services/auth.services.ts
+++ server/src/database/services/auth.services.ts
@@ -5,6 +5,7 @@ export interface UserCredentials {
   id?: number;
   name?: string;
   email?: string;
+  password?: string;
 }
 
 export const findUserByEmail = async (
@@ -12,7 +13,7 @@ export const findUserByEmail = async (
 ): Promise<UserCredentials | null> => {
   try {
     return db.oneOrNone(
-      `SELECT user_id, user_name, user_email FROM users WHERE user_email = $1`,
+      `SELECT user_id, user_name, user_email, user_password FROM users WHERE user_email = $1`,
       [email]
     );
   } catch (err) {
