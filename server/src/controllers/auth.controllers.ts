95b9958be2eb0c479dc1ee06a81c1469ea2f428a
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-27 14:23:37 -0500

    Implemented the registration controller for use in user registration

--- server/src/controllers/auth.controllers.ts
+++ server/src/controllers/auth.controllers.ts
@@ -2,8 +2,10 @@ import { RequestHandler } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
+import { createTransport } from 'nodemailer';
 
 import {
+  createUser,
   findUserByEmail,
   UserCredentials,
 } from '../database/services/auth.services';
@@ -12,9 +14,40 @@ import { HttpNotFound } from '../utils/HttpErrors/HttpNotFound';
 import { isNullish } from '../utils/isNullish';
 import { HttpUnauthorized } from '../utils/HttpErrors/HttpUnauthorized';
 import { cookieSetting } from '../utils/constants';
+import { HttpConflict } from '../utils/HttpErrors/HttpConflict';
+import { HttpInternal } from '../utils/HttpErrors/HttpInternal';
 
 const generateJwt = (payload: Partial<UserCredentials>): string =>
   jwt.sign(payload, process.env['TOKEN_SECRET'] ?? 'Tungsten Rat');
+
+const sendVerificationEmail = async (email: string) => {
+  try {
+    console.log(process.env['TRANSPORT_EMAIL'])
+    const transporter = createTransport({
+      host: 'smtp-mail.outlook.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env['TRANSPORT_EMAIL'],
+        pass: process.env['TRANSPORT_PASS'],
+      },
+      logger: true,
+      debug: true,
+    });
+    console.log('Before Sending!');
+    await transporter.sendMail({
+      from: `Loggia Web App <${process.env['TRANSPORT_EMAIL']}>`,
+      to: email,
+      subject: 'Verify your email!',
+      html: `Verify your account using this <a href='${
+        process.env['URL_ROOT']
+      }/api/auth/verify/${generateJwt({ user_email: email })}'>link</a>!`,
+    });
+    console.log('After Sending!');
+  } catch (err) {
+    throw new HttpInternal();
+  }
+};
 
 export const login: RequestHandler = asyncHandler(async (req, res, _next) => {
   const { email, password, keepLoggedIn } = req.body;
@@ -45,3 +78,32 @@ export const login: RequestHandler = asyncHandler(async (req, res, _next) => {
     email: payload.user_email,
   });
 });
+
+export const register: RequestHandler = asyncHandler(
+  async (req, res, _next) => {
+    const { name, email, password } = req.body;
+
+    if (isNullish(name, email, password)) {
+      throw new HttpBadRequest('Name, Email, or Password Missing!');
+    }
+
+    const user = await findUserByEmail(email);
+    if (user !== null) {
+      throw new HttpConflict('User already exists!');
+    }
+
+    await sendVerificationEmail(email);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userCreds: Partial<UserCredentials> = {
+      user_name: name,
+      user_email: email,
+      user_password: hashedPassword,
+    };
+    await createUser(userCreds);
+
+    res.status(201).send({
+      message: 'User registration successful!',
+    });
+  }
+);
