236f4ee93cf0c6eaf26a9d389d7a8e8450338422
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-27 16:43:04 -0500

    Implemented a database service for logbooks for creating a logbook

--- server/src/database/services/logbooks.services.ts
+++ server/src/database/services/logbooks.services.ts
@@ -15,3 +15,20 @@ export const getAllLogbooks = async (userId: number) => {
     throw new HttpInternal();
   }
 };
+
+export const createLogbook = async (logbookName: string, userId: number) => {
+  try {
+    await db.none(
+      `
+        INSERT INTO logbooks
+          (logbook_name)
+        VALUES
+          ($1)
+        WHERE user_id = $2
+    `,
+      [logbookName, userId]
+    );
+  } catch (err) {
+    throw new HttpInternal();
+  }
+};
