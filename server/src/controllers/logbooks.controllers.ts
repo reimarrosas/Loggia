deed25777cddba1b7c5317f26156770d2a9d5283
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-27 16:42:49 -0500

    Implemented the CREATE operation for logbooks

--- server/src/controllers/logbooks.controllers.ts
+++ server/src/controllers/logbooks.controllers.ts
@@ -1,7 +1,12 @@
 import { RequestHandler } from 'express';
 import asyncHandler from 'express-async-handler';
 
-import { getAllLogbooks } from '../database/services/logbooks.services';
+import {
+  createLogbook,
+  getAllLogbooks,
+} from '../database/services/logbooks.services';
+import { HttpBadRequest } from '../utils/HttpErrors/HttpBadRequest';
+import { isNullish } from '../utils/isNullish';
 
 export const getLogbooks: RequestHandler = asyncHandler(
   async (req, res, _next) => {
@@ -9,6 +14,24 @@ export const getLogbooks: RequestHandler = asyncHandler(
 
     res.send({
       logbooks: await getAllLogbooks(userId),
+    });
+  }
+);
+
+export const createLogbooks: RequestHandler = asyncHandler(
+  async (req, res, _next) => {
+    const { userId } = req.credentials;
+
+    const { logbookName } = req.body;
+
+    if (logbookName) {
+      throw new HttpBadRequest('Logbook Name Required!');
+    }
+
+    await createLogbook(logbookName, userId);
+
+    res.send({
+      message: 'Logbook successfully created!',
     });
   }
 );
