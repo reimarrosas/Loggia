ac637e99171181907378ef599f97168bfb98b9ba
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-27 12:59:52 -0500

    Modified the cookieSetting constant to become a function that depends setting maxAge based on if keepLoggedIn is true or false

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -1,9 +1,18 @@
 import { CookieOptions } from "express";
 
-export const cookieSetting: CookieOptions = {
-  maxAge: 60 * 60 * 24 * 365 * 3,
-  httpOnly: true,
-  sameSite: 'strict',
-  secure: process.env['NODE_ENV'] === 'production',
-  signed: true,
-};
+type KeepLoggedIn = 'true' | 'false';
+
+export const cookieSetting = (isKeepLoggedIn: KeepLoggedIn): CookieOptions => {
+  const options: CookieOptions = {
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env['NODE_ENV'] === 'production',
+    signed: true,
+  }
+
+  if (isKeepLoggedIn === 'true') {
+    options.maxAge = 60 * 60 * 24 * 365 * 3;
+  }
+
+  return options;
+}
