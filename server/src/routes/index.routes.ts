d8802533347c52712fce1f3d380a808f4ea9972c
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-27 11:06:33 -0500

    Mounted the auth router to the index router

--- server/src/routes/index.routes.ts
+++ server/src/routes/index.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import authRoutes from './auth.routes';
 import errorRoutes from './errors.routes';
 
 const router = express.Router()
 
+router.use('/auth', authRoutes)
 router.use(errorRoutes);
 
 export default router;
