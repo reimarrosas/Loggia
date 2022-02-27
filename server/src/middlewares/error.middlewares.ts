d12f5e1053adaa4e694b6e45853f86077b97b78d
Reimar Rosas <reimarrosas0208@gmail.com>
2022-02-27 10:09:10 -0500

    Implemented notFound handler and default error handler

---
+++ server/src/middlewares/error.middlewares.ts
@@ -0,0 +1,14 @@
+import { ErrorRequestHandler, RequestHandler } from 'express';
+import { HttpInternal } from '../utils/HttpErrors/HttpInternal';
+import { HttpNotFound } from '../utils/HttpErrors/HttpNotFound';
+
+export const notFoundHandler: RequestHandler = (req, _res, next) =>
+  next(new HttpNotFound(req.originalUrl));
+
+export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  err = err.statusCode ? err : new HttpInternal();
+
+  res.status(err.statusCode).send({
+    message: err.toString(),
+  });
+};
